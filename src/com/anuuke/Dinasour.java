337786a8d2818acaddcf087764ae3505be5b1ad2
Anuuke <xxOG_jakxx@gmail.com>
2016-09-20 11:30:11 -0500

    Created a new Dinasour and tested roar message

---
+++ src/com/anuuke/Dinasour.java
@@ -0,0 +1,12 @@
+package com.anuuke;
+
+/**
+ * Created by Anuuke on 9/20/16.
+ */
+public class Dinasour {
+    public void roar() {
+
+        System.out.println("I'm a dinasour!!!");
+        System.out.println("GRRRRR!!");
+    }
+}
---
+++ src/com/anuuke/DinasourSupervisor.java
@@ -0,0 +1,17 @@
+package com.anuuke;
+
+/**
+ * Created by Anuuke on 9/20/16.
+ */
+public class DinasourSupervisor {
+    public static void main(String[] args) {
+
+        Dinasour t_rex = new Dinasour();
+        Dinasour velociraptor = new Dinasour();
+        Dinasour Diplodocus = new Dinasour();
+
+        t_rex.roar();
+        velociraptor.roar();
+        Diplodocus.roar();
+    }
+}
